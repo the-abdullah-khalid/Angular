aab4c0c4e4d9ab8242899f516bd49b3d8727ec33
the-abdullah-khalid <abdullahkhan135790@gmail.com>
2024-07-03 12:55:33 +0500

    cmt 3 july

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,5 +7,25 @@ import { Component } from "@angular/core";
 })
 
 export class ServerComponent{
+Id:number = 1234
+serverStatus="offline"
+
+/**
+ *
+ */
+constructor() {
+    this.serverStatus=Math.random()>0.5 ? 'online' :'offline';
+    
+}
+
+getServerStatus()
+{
+    return this.serverStatus;
+}
+
+getColor()
+{
+    return this.serverStatus==='online' ? 'green' : 'red';
+}
 
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,7 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus='no server was created';
+  serverName='----';
+  serverCreated=false;
 
+  constructor() 
+  {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+
+    
+  }
+    onCreateServer()
+    {
+      debugger
+      this.serverCreated=true;
+      this.serverCreationStatus='Server is being created';
+    }
+    onUpdateServerName(event:Event)
+    {
+      this.serverName=(<HTMLInputElement>event.target).value; 
+    }
 
   
 }
