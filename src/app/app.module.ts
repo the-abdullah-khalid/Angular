3a6fa6695a671acd8cc2382e6fda361c3a58ee6c
the-abdullah-khalid <abdullahkhan135790@gmail.com>
2024-07-04 17:12:58 +0500

    databinding deepdive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
+
 import { FormsModule } from '@angular/forms';
-import { ServersComponent } from './servers/servers.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    CockpitComponent,
+    ServerElementComponent,
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent {
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output() blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer(serverContentInput:HTMLInputElement) 
+  {
+
+      this.serverCreated.emit(
+        {
+          serverName:this.newServerName,
+          serverContent:serverContentInput.value
+        }
+      );
+
+  }
+
+  onAddBlueprint(serverContentInput:HTMLInputElement) 
+  {
+    this.blueprintCreated.emit(
+      {
+        serverName:this.newServerName,
+        serverContent:serverContentInput.value
+      }
+    );
+  }
+}
